b654460a5e4f3be2094de9cc0a63bcf486d3081b
kiko98 <k.krastev.1998@abv.bg>
2020-05-30 13:02:36 +0300

    Add list selected param

--- src/app/main/admin/admin.component.ts
+++ src/app/main/admin/admin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  listSelected = true;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.listSelected = this.activatedRoute.snapshot.data['listSelected'];
+  }
 
   ngOnInit(): void {
   }
