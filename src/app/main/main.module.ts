89a4864021916b7f64f4f9dadbe773d53ff11745
kiko98 <k.krastev.1998@abv.bg>
2020-05-30 13:46:01 +0300

    Add components to declarations array

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,21 +3,30 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main-routing.module';
 import { HeaderComponent } from './layout/header/header.component';
-import { NgbRatingModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { MyFavoritesComponent } from './my-favorites/my-favorites.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '@app/shared/shared.module';
+import { AlertComponent } from '@app/shared/components/alert/alert.component';
 
 
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     NgbModule,
+    ReactiveFormsModule,
     MainRoutingModule
   ],
   declarations: [
     MainComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    MyProfileComponent,
+    MyFavoritesComponent
   ],
 })
 export class MainModule { }
