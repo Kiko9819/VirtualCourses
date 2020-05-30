d46076c92392fc1048f16c8e1034967c797f40cf
kiko98 <k.krastev.1998@abv.bg>
2020-05-30 13:47:55 +0300

    Add my favorites component

---
+++ src/app/main/my-favorites/my-favorites.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../courses/services/courses.service';
+import { Course } from '../courses/models/course.interface';
+import { take } from 'rxjs/operators';
+import { AccountService } from '@app/core/services/account.service';
+
+@Component({
+  selector: 'app-my-favorites',
+  templateUrl: './my-favorites.component.html',
+  styleUrls: ['./my-favorites.component.scss']
+})
+export class MyFavoritesComponent implements OnInit {
+
+  courses: Course[];
+
+  constructor(private coursesService: CoursesService,
+              private accountsService: AccountService,
+              private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.coursesService.getFavorites(this.accountsService.userValue.email).pipe(
+        take(1)
+      ).subscribe(courses => this.courses = courses);
+  }
+
+  get email(): string {
+    return this.accountService.userEmail;
+  }
+}
