2a070bb2ba023c975fde9b85b689f9db3905774a
kiko98 <k.krastev.1998@abv.bg>
2020-05-30 13:33:32 +0300

    Change URL and add addToFav

--- src/app/main/courses/services/courses.service.ts
+++ src/app/main/courses/services/courses.service.ts
@@ -2,33 +2,51 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Course} from '@app/main/courses/models/course.interface';
-import {MainModule} from '@app/main/main.module';
+import { environment } from '@environments/environment';
+import { map } from 'rxjs/operators';
+import { AccountService } from '@app/core/services/account.service';
+import { Favorite } from '@app/core/models/favorite';
 
 @Injectable({
-  providedIn: MainModule
+  providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor(private httpClient: HttpClient) { }
+  readonly CORE_URL = `${environment.apiUrl}`;
+  readonly COURSES_URL = `${environment.apiUrl}/courses`;
+
+  constructor(private httpClient: HttpClient, private accountsService: AccountService) { }
+
+
+  getFavorites(email: string): Observable<any> {
+    return this.httpClient.get(`${this.CORE_URL}/favorites`).pipe(
+      map((data: any) => {
+        data = data.filter((x: Favorite) => {
+          return x[email];
+        });
+        return data;
+      })
+    );
+  }
 
   getAll(): Observable<any> {
-    return this.httpClient.get('api_url/courses');
+    return this.httpClient.get(this.COURSES_URL);
   }
 
   getById(courseId: string | number): Observable<any> {
-    return this.httpClient.get(`api_url/${courseId}`);
+    return this.httpClient.get(`${this.COURSES_URL}/${courseId}`);
   }
 
   create(course: Course): Observable<any> {
-    return this.httpClient.post('api_url', {course});
+    return this.httpClient.post(this.COURSES_URL, {course});
   }
 
   update(course: Course): Observable<any> {
-    return this.httpClient.patch('api_url', {course});
+    return this.httpClient.patch(this.COURSES_URL, {course});
   }
 
   delete(courseId: string | number): Observable<any> {
-    return this.httpClient.delete(`api_url/${courseId}`);
+    return this.httpClient.delete(`${this.COURSES_URL}/${courseId}`);
   }
 
   save(course: Course): any {
@@ -37,5 +55,14 @@ export class CoursesService {
     } else {
       return this.create(course);
     }
+  }
+
+  addToFavorites(course: Course): any {
+    const user = this.accountsService.userValue;
+    const body = {
+      [user.email]: course
+    };
+
+    return this.httpClient.post(`${this.CORE_URL}/favorites`, body);
   }
 }
