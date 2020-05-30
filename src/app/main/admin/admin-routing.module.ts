d7091e714952be8e049217a35d738091f7917bff
kiko98 <k.krastev.1998@abv.bg>
2020-05-30 13:03:57 +0300

    Add courses module

--- src/app/main/admin/admin-routing.module.ts
+++ src/app/main/admin/admin-routing.module.ts
@@ -2,10 +2,24 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AdminComponent} from '@app/main/admin/admin.component';
 
+const coursesModule = () => import('../courses/courses.module').then(m => m.CoursesModule);
+
 const routes: Routes = [
   {
     path: '',
-    component: AdminComponent
+    component: AdminComponent,
+
+    children: [
+      {
+        path: 'courses',
+        loadChildren: coursesModule,
+        data: {
+          manageCourses: true
+        }
+        // TODO: Make sure to fix this
+        // canLoad: [AdminGuard]
+      },
+    ]
   }
 ];
 
