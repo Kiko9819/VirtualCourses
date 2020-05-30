adff7e5a5bd6743573b7b85689bbf0dfa628b5c6
kiko98 <k.krastev.1998@abv.bg>
2020-05-30 13:32:11 +0300

    Add to favorites

--- src/app/main/courses/courses.component.ts
+++ src/app/main/courses/courses.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import { CoursesService } from './services/courses.service';
+import { take } from 'rxjs/operators';
+import { Course } from './models/course.interface';
 
 @Component({
   selector: 'app-courses',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+
+  courses: Course[];
+  manageCourses: boolean;
+  // TODO Remove, once rating is implemented
   currentRate = 6;
-  constructor() { }
+
+  constructor(private route: ActivatedRoute, private coursesService: CoursesService) {
+    this.manageCourses = this.route.snapshot.data['manageCourses'];
+  }
 
   ngOnInit(): void {
+    this.coursesService.getAll().pipe(
+      take(1)
+    ).subscribe(courses => this.courses = courses);
+  }
+
+  addToFav(course: Course): void {
+    this.coursesService.addToFavorites(course).pipe(
+      take(1)
+    ).subscribe(data => console.log(data));
   }
 
 }
