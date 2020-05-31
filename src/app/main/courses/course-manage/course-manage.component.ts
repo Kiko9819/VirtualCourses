5e733b0567f67e2842cb0a7136e018fee927d4b8
kiko <k.krastev.1998@gmail.com>
2020-05-31 18:54:25 +0300

    Add navigate

--- src/app/main/courses/course-manage/course-manage.component.ts
+++ src/app/main/courses/course-manage/course-manage.component.ts
@@ -1,29 +1,23 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Course} from '@app/main/courses/models/course.interface';
-import {Favorite} from '@app/core/models/favorite';
-import {CoursesService} from "@app/main/courses/services/courses.service";
-import {take} from "rxjs/operators";
+import {CoursesService} from '@app/main/courses/services/courses.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-course-manage',
   templateUrl: './course-manage.component.html',
   styleUrls: ['./course-manage.component.scss']
 })
-export class CourseManageComponent implements OnInit {
+export class CourseManageComponent {
 
   @Input() course: Course;
   @Output() delete: EventEmitter<number> = new EventEmitter<number>();
 
-  constructor(private coursesService: CoursesService) {
-  }
-
-  ngOnInit(): void {
+  constructor(private coursesService: CoursesService, private router: Router) {
   }
 
   editCourse(courseId: number): void {
-    console.log(courseId);
-    // navigate and pass in id
-    // the edit/create component will load the course by id
+    this.router.navigate(['/admin/courses/' + courseId]);
   }
 
   deleteCourse(courseId: number): void {
