9d93f3520cdc6caf00d9ad1b6cf50f81306ff5ae
kiko <k.krastev.1998@gmail.com>
2020-05-31 18:55:35 +0300

    Remove commented code

--- src/app/main/courses/courses-routing.module.ts
+++ src/app/main/courses/courses-routing.module.ts
@@ -1,23 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CoursesComponent } from '@app/main/courses/courses.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CoursesComponent} from '@app/main/courses/courses.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: CoursesComponent,
-
-    // children: [
-    //   {
-    //     path: '',
-    //     redirectTo: 'courses',
-    //     pathMatch: 'full'
-    //   },
-    //   {
-    //     path: 'courses',
-    //     component: CoursesComponent
-    //   }
-    // ]
+    component: CoursesComponent
   }
 ];
 
@@ -25,4 +13,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class CoursesRoutingModule { }
+export class CoursesRoutingModule {
+}
