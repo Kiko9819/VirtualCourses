74f6c4273d6590c250b1a6a23ff072152f59a8f0
kiko <k.krastev.1998@gmail.com>
2020-05-31 18:56:16 +0300

    Submit handler

--- src/app/main/my-profile/my-profile.component.ts
+++ src/app/main/my-profile/my-profile.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { AccountService } from '@app/core/services/account.service';
-import { AlertService } from '@app/core/services/alert.service';
-import { User } from '@app/core/models/user';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AccountService} from '@app/core/services/account.service';
+import {AlertService} from '@app/core/services/alert.service';
+import {User} from '@app/core/models/user';
 import {take} from 'rxjs/operators';
 
 @Component({
@@ -21,44 +21,53 @@ export class MyProfileComponent implements OnInit {
   constructor(private fb: FormBuilder,
               private accountService: AccountService,
               private alertService: AlertService) {
-      this.user = this.accountService.userValue;
-    }
+    this.user = this.accountService.userValue;
+  }
 
   ngOnInit(): void {
     this.buildForm();
   }
 
-   // convenience getter for easy access to form fields
-   get f() { return this.formGroup.controls; }
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.formGroup.controls;
+  }
 
-   onSubmit() {
-       this.submitted = true;
+  onSubmit() {
+    this.submitted = true;
 
-       // reset alerts on submit
-       this.alertService.clear();
+    // reset alerts on submit
+    this.alertService.clear();
 
-       // stop here if form is invalid
-       if (this.formGroup.invalid) {
-           return;
-       }
+    // stop here if form is invalid
+    if (this.formGroup.invalid) {
+      return;
+    }
 
-       this.loading = true;
-       this.accountService.update(this.user.id, this.formGroup.value).pipe(
-         take(1)
-       ).subscribe(() => {
-         this.loading = false;
-         this.alertService.success('Successfully updated your profile!');
-       }, () => {
-         this.alertService.error('Oops... something went wrong');
-       });
-   }
+    this.loading = true;
+    this.accountService.update(this.user.id, this.formGroup.value).pipe(
+      take(1)
+    ).subscribe(() => {
+      this.loading = false;
+      this.alertService.success('Successfully updated your profile!');
+      this.clearPasswordField();
+    }, () => {
+      this.loading = false;
+      this.alertService.error('Oops... something went wrong');
+    });
+  }
 
   private buildForm(): void {
     this.formGroup = this.fb.group({
       firstName: [this.accountService.userValue.firstName, Validators.required],
       lastName: [this.accountService.userValue.lastName, Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]]
-    })
+    });
+  }
+
+  private clearPasswordField(): void {
+    this.formGroup.controls['password'].setValue('');
+    this.formGroup.controls['password'].setErrors(null);
   }
 
 }
