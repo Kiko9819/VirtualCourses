a26ec7313c6e2670bdd780f60a43b3616ed409b5
kiko <k.krastev.1998@gmail.com>
2020-05-31 18:54:55 +0300

    Add modules and more components

--- src/app/main/courses/courses.module.ts
+++ src/app/main/courses/courses.module.ts
@@ -5,13 +5,18 @@ import {CoursesRoutingModule} from '@app/main/courses/courses-routing.module';
 import {NgbRatingModule} from '@ng-bootstrap/ng-bootstrap';
 import { CourseComponent } from './course/course.component';
 import { CourseManageComponent } from './course-manage/course-manage.component';
+import { CourseEditComponent } from './course-edit/course-edit.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {SharedModule} from "@app/shared/shared.module";
 
 
 
 @NgModule({
-  declarations: [CoursesComponent, CourseComponent, CourseManageComponent],
+  declarations: [CoursesComponent, CourseComponent, CourseManageComponent, CourseEditComponent],
   imports: [
     NgbRatingModule,
+    SharedModule,
+    ReactiveFormsModule,
     CommonModule,
     CoursesRoutingModule
   ]
