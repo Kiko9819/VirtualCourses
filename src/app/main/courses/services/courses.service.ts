7eb0d71adb90ed373b12baea349e8fb90a2df881
kiko <k.krastev.1998@gmail.com>
2020-06-01 01:19:11 +0300

    Add rating create and update

--- src/app/main/courses/services/courses.service.ts
+++ src/app/main/courses/services/courses.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Course} from '@app/main/courses/models/course.interface';
-import { environment } from '@environments/environment';
-import { map } from 'rxjs/operators';
-import { AccountService } from '@app/core/services/account.service';
-import { Favorite } from '@app/core/models/favorite';
+import {environment} from '@environments/environment';
+import {map} from 'rxjs/operators';
+import {AccountService} from '@app/core/services/account.service';
+import {Favorite} from '@app/core/models/favorite';
+import {Rating} from '@app/core/models/rating';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,8 @@ export class CoursesService {
   readonly CORE_URL = `${environment.apiUrl}`;
   readonly COURSES_URL = `${environment.apiUrl}/courses`;
 
-  constructor(private httpClient: HttpClient, private accountsService: AccountService) { }
+  constructor(private httpClient: HttpClient, private accountsService: AccountService) {
+  }
 
   removeFromFavorites(favoriteId: number): Observable<any> {
     return this.httpClient.delete(`${this.CORE_URL}/favorites/${favoriteId}`);
@@ -36,6 +38,10 @@ export class CoursesService {
     return this.httpClient.get(this.COURSES_URL);
   }
 
+  getRatings(): Observable<any> {
+    return this.httpClient.get(`${this.CORE_URL}/ratings`);
+  }
+
   getById(courseId: string | number): Observable<any> {
     return this.httpClient.get(`${this.COURSES_URL}/${courseId}`);
   }
@@ -50,6 +56,24 @@ export class CoursesService {
 
   delete(courseId: string | number): Observable<any> {
     return this.httpClient.delete(`${this.COURSES_URL}/${courseId}`);
+  }
+
+  saveCourseRating(rating: Rating): Observable<any> {
+    const ratingExists = () => {
+      return (rating.id || rating.id === 0);
+    };
+    if (ratingExists()) {
+      return this.updateRating(rating);
+    }
+    return this.createRating(rating);
+  }
+
+  createRating(rating: Rating): Observable<any> {
+    return this.httpClient.post(`${this.CORE_URL}/ratings`, rating);
+  }
+
+  updateRating(rating: Rating) {
+    return this.httpClient.put(`${this.CORE_URL}/ratings/${rating.id}`, rating);
   }
 
   saveCourse(course: Course): any {
